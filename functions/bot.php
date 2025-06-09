e39ecea42f749f9c0d0e155394c57554602b4b42
Awmtea Polythene <omomacck@yahoo.com>
2025-06-09 14:41:27 +0530

    Update bot.php

--- functions/bot.php
+++ functions/bot.php
@@ -3,7 +3,7 @@
 ///////////===[Bot Functions]===///////////
 function bot($method,$datas=[]){
     global $config;
-    $url = "https://api.telegram.org/bot".$config['botToken']."/".$method;
+    $url = "https://api.telegram.org/bot".$config['7175703585:AAHMGHu_ggfNh1D5EZakddJ0OeKK80AI5uI']."/".$method;
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
